31084e2ee376c0ad0d45646ad2cb1bec73a42525
Yisaaaa <ryansanisit19@gmail.com>
2024-09-07 16:27:39 +0800

    Forgot to add code documentation for method JoinDirectory

--- GitLite/src/Repository.cs
+++ GitLite/src/Repository.cs
@@ -4,12 +4,15 @@ public class Repository
 {
     
     private static DirectoryInfo CWD = Directory.GetParent(AppContext.BaseDirectory);
-    
+    private static DirectoryInfo GITLITE_DIR = Utils.JoinDirectory(CWD, ".gitlite");
     /// <summary>
     /// This initializes the GitLite repository on the Current Working Directory.
     /// </summary>
     public static void Init()
     {
+        // TODO: Include other folders such as commits, blobs, etc.
+        
+        GITLITE_DIR.Create();
         Console.WriteLine($"Initialized GitLite at {CWD.ToString()}");
     } 
 }
--- GitLite/src/Utils.cs
+++ GitLite/src/Utils.cs
@@ -5,12 +5,26 @@ public class Utils
     
     
     /* FILE & DIR UTILS*/
-
+  
+    /// <summary>
+    /// Joins the first and second directory path together.
+    /// </summary>
+    /// <param name="first">String representation of the first path</param>
+    /// <param name="second">String representation of the second path to add
+    /// to the FIRST path</param>
+    /// <returns>A DirectoryInfo referencing to the joined directory.</returns>
     public static DirectoryInfo JoinDirectory(string first, string second)
     {
         return new DirectoryInfo(Path.Combine(first, second));
     }
 
+    /// <summary>
+    /// Joins a DirectoryInfo with a string together.
+    /// </summary>
+    /// <param name="first">DirectoryInfo representation of the first path</param>
+    /// <param name="second">String representation of the second path to add
+    /// to the FIRST path</param>
+    /// <returns>A DirectoryInfo referencing to the joined directory.</returns>
     public static DirectoryInfo JoinDirectory(DirectoryInfo first, string second)
     {
         return JoinDirectory(first.ToString(), second);
@@ -33,7 +47,13 @@ public class Utils
         Environment.Exit(-1);
     }
     
-    
+    /// <summary>
+    /// Checks if the number of arguments provided with CMD(command) is valid.
+    /// </summary>
+    /// <param name="cmd">Command</param>
+    /// <param name="args">The arguments provided along with the COMMAND</param>
+    /// <param name="n">The number of arguments required.</param>
+    /// <exception cref="ArgumentException">Returns an ArgumentException if invalid.</exception>
     public static void ValidateArguments(string cmd, string[] args, int n)
     {
         if (args.Length != n)
