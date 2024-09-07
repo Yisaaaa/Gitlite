57447b1ca237bce5bc539f63779a869cb0168638
Yisaaaa <ryansanisit19@gmail.com>
2024-09-07 16:21:14 +0800

    Add util method ti join directories

--- GitLite/src/Utils.cs
+++ GitLite/src/Utils.cs
@@ -2,6 +2,23 @@ namespace GitLite;
 
 public class Utils
 {
+    
+    
+    /* FILE & DIR UTILS*/
+
+    public static DirectoryInfo JoinDirectory(string first, string second)
+    {
+        return new DirectoryInfo(Path.Combine(first, second));
+    }
+
+    public static DirectoryInfo JoinDirectory(DirectoryInfo first, string second)
+    {
+        return JoinDirectory(first.ToString(), second);
+    }
+    
+    
+    /* MESSAGES & ERROR REPORTING*/
+    
     /// <summary>
     /// Exits the program and prints out MESSAGE.
     /// </summary>
