9219e0b05aeb9ea9cc3b97df3d1160bca19f53c4
Yisaaaa <ryansanisit19@gmail.com>
2024-09-09 21:18:31 +0800

    Update on init method. Added creation of blobs and commits dir

--- GitLite/src/Repository.cs
+++ GitLite/src/Repository.cs
@@ -8,15 +8,29 @@ public class Repository
     
     private static DirectoryInfo CWD = Directory.GetParent(AppContext.BaseDirectory);
     private static DirectoryInfo GITLITE_DIR = Utils.JoinDirectory(CWD, ".gitlite");
+    private static DirectoryInfo COMMITS_DIR = Utils.JoinDirectory(GITLITE_DIR, "commits");
+    private static DirectoryInfo BLOBS_DIR = Utils.JoinDirectory(GITLITE_DIR, "blobs");
     
     /// <summary>
     /// This initializes the GitLite repository on the Current Working Directory.
     /// </summary>
     public static void Init()
     {
-        // TODO: Include other folders such as commits, blobs, etc.
+        // Check if .gitlite folder already exists
+        if (GitliteAlreadyInitialized())
+        {
+            Utils.ExitWithError("A Gitlet version-control system already exists in the current directory.");
+        }
         
         GITLITE_DIR.Create();
-        Console.WriteLine($"Initialized GitLite at {CWD.ToString()}");
-    } 
+        COMMITS_DIR.Create();
+        BLOBS_DIR.Create();
+        
+        Console.WriteLine($"Initialized a new GitLite at {CWD.ToString()}");
+    }
+
+    private static bool GitliteAlreadyInitialized()
+    {
+        return GITLITE_DIR.Exists;
+    }
 }
