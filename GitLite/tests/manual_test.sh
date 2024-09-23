1a71ce4300434dc6926190d4c1423c6c6d5261ea
Yisaaaa <ryansanisit19@gmail.com>
2024-09-23 18:44:42 +0800

    Update: deletes all other files aside from .gitlite directory

--- GitLite/tests/manual_test.sh
+++ GitLite/tests/manual_test.sh
@@ -2,5 +2,6 @@ BUILD="/home/luis/RiderProjects/GitLite/GitLite/bin/Release/net6.0/linux-x64/pub
 cd ../
 dotnet publish -c Release -r linux-x64 --self-contained -p:PublishSingleFile=true
 cd ~/gitlite
-rm -rf .gitlite
+rm -rf * .gitlite
 cp $BUILD ./
+./GitLite init
