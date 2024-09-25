8fffc2bacb71f2060d1e4d979cb28fb5ec558874
Yisaaaa <ryansanisit19@gmail.com>
2024-09-25 17:46:11 +0800

    Added code documentation

--- Gitlite/src/Reader.cs
+++ Gitlite/src/Reader.cs
@@ -5,6 +5,12 @@ namespace GitLite;
 
 public class Reader
 {
+    
+    /// <summary>
+    /// Reads an object like Commit, Blob, etc. Only for testing purposes.
+    /// </summary>
+    /// <param name="fileName">File name on the object to read.</param>
+    /// <param name="objectType">Type of the object to read. (e.g. Commit, Blob)</param>
     public static void Read(string fileName, string objectType)
     {
         switch (objectType)
@@ -19,11 +25,13 @@ public class Reader
                 string fileContents = System.Text.Encoding.UTF8.GetString(bytes);
                 Console.WriteLine(fileContents);
                 break;
+            
             case "staging":
                 StagingArea staging = StagingArea.GetDeserializedStagingArea();
                 Console.WriteLine("staging");
                 staging.StringValue();
                 break;
+            
             default:
                 Utils.ExitWithError($"No object with name: {objectType}");
                 break;
