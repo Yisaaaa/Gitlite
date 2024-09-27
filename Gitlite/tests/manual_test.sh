483715f99b568f3e09735c77caa593a89db14471
Yisaaaa <ryansanisit19@gmail.com>
2024-09-27 17:43:46 +0800

    Removed the previous tests written on bash

--- Gitlite/tests/manual_test.sh
+++
@@ -1,7 +0,0 @@
-BUILD="/home/luis/RiderProjects/Gitlite/Gitlite/bin/Release/net6.0/linux-x64/publish/Gitlite"
-cd ../
-dotnet publish -c Release -r linux-x64 --self-contained -p:PublishSingleFile=true
-cd ~/gitlite
-rm -rf * .gitlite
-cp $BUILD ./
-./Gitlite init
